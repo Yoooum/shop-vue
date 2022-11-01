5bfbe6098e5adad8f1c8732002f1a3c94a4dccee
Yoooum <akyomi@qq.com>
2022-11-01 08:52:22 +0800

    copy一个aside

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,8 +18,48 @@ const logout = () => {
 </script>
 
 <template>
+  <h1 class="text-4xl font-bold">Welcome to the Shop</h1>
+  <aside class="w-50">
+    
+  <el-row class="tac">
+    <el-col>
+      <h5 class="mb-2">Default colors</h5>
+      <el-menu>
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+      </el-col>
+      </el-row>
+  </aside>
   <main>
-    <h1 class="text-4xl font-bold">Welcome to the Shop</h1>
+    
     <br />
     <!-- 显示状态 -->
     {{ cookies.get('token') }}
